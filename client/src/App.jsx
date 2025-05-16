b6143c19d60c56f49f72560c2a49d80eaca4f060
Diluka Vithanage <diluka.20200629@iit.ac.lk>
2025-05-16 20:58:45 +0530

    client update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
-function App() {
-  const [count, setCount] = useState(0)
+// Components
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import ProtectedRoute from './components/ProtectedRoute';
 
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import BlogDetail from './pages/BlogDetail';
+import CreateBlog from './pages/CreateBlog';
+import EditBlog from './pages/EditBlog';
+import Profile from './pages/Profile';
+import CountryBlogs from './pages/CountryBlogs';
+import UserBlogs from './pages/UserBlogs';
+import FollowingFeed from './pages/FollowingFeed';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound';
+
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navigation />
+      <main className="page-container">
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/blogs/:id" element={<BlogDetail />} />
+            <Route path="/country/:name" element={<CountryBlogs />} />
+            <Route path="/user/:id/blogs" element={<UserBlogs />} />
+            <Route path="/search" element={<Search />} />
+            
+            {/* Protected Routes */}
+            <Route path="/create-blog" element={
+              <ProtectedRoute>
+                <CreateBlog />
+              </ProtectedRoute>
+            } />
+            <Route path="/edit-blog/:id" element={
+              <ProtectedRoute>
+                <EditBlog />
+              </ProtectedRoute>
+            } />
+            <Route path="/profile/:id" element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            } />
+            <Route path="/following" element={
+              <ProtectedRoute>
+                <FollowingFeed />
+              </ProtectedRoute>
+            } />
+            
+            {/* 404 Route */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App; 
